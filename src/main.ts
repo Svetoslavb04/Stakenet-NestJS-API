9faba656abe517954f2ccaaf843f22014806459a
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-11 19:06:33 +0300

    feat: app PORT should be configurated in .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+
+  await app.listen(config.get('PORT') || 3333);
 }
 bootstrap();
