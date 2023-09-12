1a77ab179effd19f4d0bfd62bbd9592c8397a3ac
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 17:27:02 +0300

    feat: add getInfuraProvider function in Web3Service

--- src/web3/web3.service.ts
+++ src/web3/web3.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Networkish, ethers } from 'ethers';
 
 @Injectable()
 export class Web3Service {
   constructor(private config: ConfigService) {}
+
+  getInfuraProvider(network: Networkish) {
+    return new ethers.InfuraProvider(
+      network,
+      this.config.get('INFURA_API_KEY') || null,
+      this.config.get('INFURA_API_KEY_SECRET') || null,
+    );
+  }
 }
