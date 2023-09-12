9d5648f03fc7885526aef20e506621adcd7fd30e
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 17:16:09 +0300

    feat: add index route with param validation

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { EthereumAddress } from './dto/ethereumAdress.dto';
 
 @Controller('token')
-export class TokenController {}
+export class TokenController {
+  @Get(':address')
+  index(@Param('') params: EthereumAddress) {
+    return params;
+  }
+}
