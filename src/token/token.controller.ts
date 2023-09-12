ab2d60ebfd9462dd85c13adbe9236b4aee04dddb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 19:13:32 +0300

    feat: token info endpoint now working

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { EthereumAddress } from './dto/ethereumAdress.dto';
 import { Web3Service } from 'src/web3/web3.service';
+import { ERC20, ERC20__factory } from 'types/ethers-contracts';
 
 @Controller('token')
 export class TokenController {
   constructor(private web3: Web3Service) {}
 
   @Get(':address')
-  index(@Param('') params: EthereumAddress) {
-    return params;
+  async index(@Param('') params: EthereumAddress) {
+    try {
+      const token = this.web3.getContractAt<ERC20__factory, ERC20>(
+        ERC20__factory,
+        params.address,
+      );
+
+      const name = await token.name();
+      const symbol = await token.symbol();
+      const decimalsBigIng = await token.decimals();
+
+      return { name, symbol, decimals: decimalsBigIng.toString() };
+    } catch (error) {
+      throw new BadRequestException('Ethereum address is not an ERC20 token');
+    }
   }
 }
