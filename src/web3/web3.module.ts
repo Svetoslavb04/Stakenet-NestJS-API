6fe3d1ddc455744b463c83be6ccb822f87134bcf
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 17:17:45 +0300

    feat: web3 module now global

--- src/web3/web3.module.ts
+++ src/web3/web3.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { Web3Service } from './web3.service';
 
+@Global()
 @Module({
-  providers: [Web3Service]
+  providers: [Web3Service],
 })
 export class Web3Module {}
