1ab210233d832c51d9d41688a32300b10f4e44da
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 17:18:20 +0300

    feat: web3 service injects ConfigService

--- src/web3/web3.service.ts
+++ src/web3/web3.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class Web3Service {}
+export class Web3Service {
+  constructor(private config: ConfigService) {}
+}
