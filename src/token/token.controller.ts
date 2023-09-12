dc4a23064671c9f4dd6b225d1aeb4923e1f4ad53
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-12 19:09:52 +0300

    feat: inject web3 service in token controller

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { EthereumAddress } from './dto/ethereumAdress.dto';
+import { Web3Service } from 'src/web3/web3.service';
 
 @Controller('token')
 export class TokenController {
+  constructor(private web3: Web3Service) {}
+
   @Get(':address')
   index(@Param('') params: EthereumAddress) {
     return params;
