975600340ef05e7fd52f7bac1ec25651d3ab6c75
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-13 18:19:16 +0300

    feat: initialize stakenet index path

--- src/stakenet/stakenet.controller.ts
+++ src/stakenet/stakenet.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { EthereumAddressDTO } from 'src/dto/ethereumAdress.dto';
 
 @Controller('contract')
-export class StakenetController {}
+export class StakenetController {
+  @Get(':address')
+  async index(@Param('') params: EthereumAddressDTO) {
+    return null;
+  }
+}
