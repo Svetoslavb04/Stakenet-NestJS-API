8c197298139c79f12b37c81a04b415f1b694ae36
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-13 16:16:23 +0300

    feat: use TokenService in TokenController

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,25 +1,16 @@
 import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { EthereumAddressDTO } from './dto/ethereumAdress.dto';
-import { Web3Service } from 'src/web3/web3.service';
-import { ERC20, ERC20__factory } from 'types/ethers-contracts';
+import { TokenService } from './token.service';
 
 @Controller('token')
 export class TokenController {
-  constructor(private web3: Web3Service) {}
+  constructor(private token: TokenService) {}
 
   @Get(':address')
   async index(@Param('') params: EthereumAddressDTO) {
     try {
-      const token = this.web3.getContractAt<ERC20__factory, ERC20>(
-        ERC20__factory,
-        params.address,
-      );
-
-      const name = await token.name();
-      const symbol = await token.symbol();
-      const decimals = await token.decimals();
-
-      return { name, symbol, decimals: decimals.toString() };
+      const tokenInformation = await this.token.getTokenBaseInformation(params);
+      return tokenInformation;
     } catch (error) {
       throw new BadRequestException('Ethereum address is not an ERC20 token');
     }
