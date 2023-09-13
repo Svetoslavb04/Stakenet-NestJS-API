3a01171dff7450e6d721cf66f56c5e2c6cd28e13
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-13 18:15:47 +0300

    feat: create stakenet module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TokenModule } from './token/token.module';
 import { Web3Module } from './web3/web3.module';
+import { StakenetModule } from './stakenet/stakenet.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { Web3Module } from './web3/web3.module';
     }),
     TokenModule,
     Web3Module,
+    StakenetModule,
   ],
   controllers: [],
   providers: [],
