c2b45298f0996baa35405332a4328752e79a6034
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-13 16:02:35 +0300

    feat: inject Web3Service in TokenService

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Web3Service } from 'src/web3/web3.service';
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+  constructor(private web3: Web3Service) {}
+}
