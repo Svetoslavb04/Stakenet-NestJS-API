931af0b3e3cd658c9e37898658b87a2fb83f50c4
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-13 18:16:37 +0300

    feat: create stakenet controller

--- src/stakenet/stakenet.module.ts
+++ src/stakenet/stakenet.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { StakenetController } from './stakenet.controller';
 
-@Module({})
+@Module({
+  controllers: [StakenetController]
+})
 export class StakenetModule {}
