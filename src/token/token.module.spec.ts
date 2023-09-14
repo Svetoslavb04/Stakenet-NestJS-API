25f081c4fd8397e654bf62d94cb3afa03cfd68e6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-14 16:43:12 +0300

    feat: test token module

---
+++ src/token/token.module.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TokenModule } from './token.module';
+import { TokenController } from './token.controller';
+import { TokenService } from './token.service';
+import { Web3Module } from '../web3/web3.module';
+import { ConfigModule } from '@nestjs/config';
+
+describe('TokenModule', () => {
+  let tokenModule: TestingModule;
+
+  beforeEach(async () => {
+    tokenModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        Web3Module,
+        TokenModule,
+      ],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    expect(tokenModule).toBeDefined();
+  });
+
+  it('should have the TokenController', () => {
+    const tokenController = tokenModule.get<TokenController>(TokenController);
+    expect(tokenController).toBeDefined();
+  });
+
+  it('should have the TokenService', () => {
+    const tokenService = tokenModule.get<TokenService>(TokenService);
+    expect(tokenService).toBeDefined();
+  });
+});
