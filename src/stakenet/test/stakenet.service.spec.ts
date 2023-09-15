b9917a99a4868322677b427c21ccb90491f6dc7a
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-15 16:30:46 +0300

    feat: move StakenetService test file

--- src/stakenet/test/stakenet.service.spec.ts
+++ src/stakenet/test/stakenet.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { StakenetService } from './stakenet.service';
+import { StakenetService } from '../stakenet.service';
 
 describe('StakenetService', () => {
   let service: StakenetService;
