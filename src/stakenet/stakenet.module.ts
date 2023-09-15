40986e5e0e0516e96394b8395058fcb4fbadac0f
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-15 16:29:37 +0300

    feat: create StakenetService

--- src/stakenet/stakenet.module.ts
+++ src/stakenet/stakenet.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StakenetController } from './stakenet.controller';
+import { StakenetService } from './stakenet.service';
 
 @Module({
-  controllers: [StakenetController]
+  controllers: [StakenetController],
+  providers: [StakenetService]
 })
 export class StakenetModule {}
---
+++ src/stakenet/stakenet.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { StakenetService } from './stakenet.service';
+
+describe('StakenetService', () => {
+  let service: StakenetService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [StakenetService],
+    }).compile();
+
+    service = module.get<StakenetService>(StakenetService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
