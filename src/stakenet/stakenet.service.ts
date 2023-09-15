067e0024570ea460aff856b8010a84aec2be338b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-15 18:00:29 +0300

    feat: create stakenet service skeleton

--- src/stakenet/stakenet.service.ts
+++ src/stakenet/stakenet.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Web3Service } from 'src/web3/web3.service';
 
 @Injectable()
-export class StakenetService {}
+export class StakenetService {
+  constructor(private web3: Web3Service) {}
+
+  async getPropertyData(address: string, property: string) {}
+
+  async getPropertyWithUserRequired(
+    address: string,
+    property: string,
+    user: string,
+  ) {}
+
+  async getPropertyWithOwnerAndSpender(
+    address: string,
+    property: string,
+    owner: string,
+    spender: string,
+  ) {}
+
+  async getUserData(address: string, user: string) {}
+}
