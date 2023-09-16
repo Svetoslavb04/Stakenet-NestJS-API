00aa05ef357f0227cb4cec3585b6b047f594ab54
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-16 15:08:15 +0300

    feat: FormatDataInterceptor now handles seconds

--- src/stakenet/interceptor/format-data.interceptor.ts
+++ src/stakenet/interceptor/format-data.interceptor.ts
@@ -22,6 +22,49 @@ export class FormatDataInterceptor<T>
   ): Observable<Response<T>> {
     return next.handle().pipe(
       map((data) => {
+        const args = context.getArgs();
+        const { property } = args[0].query;
+        if (!property) {
+          return { data: typeof data === 'bigint' ? data.toString() : data };
+        }
+
+        //Data type is seconds
+        if ([StakenetPropertyEnum.lockDurationInSeconds].includes(property)) {
+          const lockDurationInSeconds: bigint = data;
+
+          let result: {
+            data: undefined;
+            lockDurationIn: {
+              days: number | string;
+              hours: number | string;
+              minutes: number | string;
+              seconds: number | string;
+              milliseconds: number | string;
+            };
+          } = {
+            data: undefined,
+            lockDurationIn: {
+              days: (lockDurationInSeconds / 86400n).toString(),
+              hours: (lockDurationInSeconds / 3600n).toString(),
+              minutes: (lockDurationInSeconds / 60n).toString(),
+              seconds: lockDurationInSeconds.toString(),
+              milliseconds: (lockDurationInSeconds * 1000n).toString(),
+            },
+          };
+
+          if (lockDurationInSeconds <= Number.MAX_SAFE_INTEGER) {
+            const lockDurationInSecondsNumber = Number(lockDurationInSeconds);
+            result.lockDurationIn.days = lockDurationInSecondsNumber / 86400;
+            result.lockDurationIn.hours = lockDurationInSecondsNumber / 3600;
+            result.lockDurationIn.minutes = lockDurationInSecondsNumber / 60;
+            result.lockDurationIn.seconds = lockDurationInSecondsNumber;
+            result.lockDurationIn.milliseconds =
+              lockDurationInSecondsNumber * 1000;
+          }
+
+          return result;
+        }
+
         return { data: typeof data === 'bigint' ? data.toString() : data };
       }),
     );
