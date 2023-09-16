e6cc502730f91b8ef88baf6a960b726f089776b1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-16 12:03:29 +0300

    feat: add contract controller logic for index endpoint

--- src/stakenet/stakenet.controller.ts
+++ src/stakenet/stakenet.controller.ts
@@ -1,14 +1,67 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { EthereumAddressDTO } from '../dto/ethereumAdress.dto';
 import { ContractDataQuery } from './dto/contract-data-query.dto';
+import { StakenetService } from './stakenet.service';
+import { StakenetPropertyWithRequiredUser } from './enum/stakenet-property-with-required-user.enum';
+import { StakenetPropertyWithOwner } from './enum/stakenet-property-with-owner.enum';
+import { StakenetPropertyWithSpender } from './enum/stakenet-property-with-spender.enum';
+import { StakenetPropertyEnum } from './enum/stakenet-property.enum';
 
 @Controller('contract')
 export class StakenetController {
+  constructor(private stakenet: StakenetService) {}
+
   @Get(':address')
   async index(
     @Param('') params: EthereumAddressDTO,
     @Query('') contractDataQuery: ContractDataQuery,
   ) {
-    return { params, contractDataQuery };
+    const { address } = params;
+    const { property, user, owner, spender } = contractDataQuery;
+
+    try {
+      if (
+        !(property in StakenetPropertyWithRequiredUser) &&
+        !(property in StakenetPropertyWithOwner) &&
+        !(property in StakenetPropertyWithSpender)
+      ) {
+        return await this.stakenet.getPropertyData(address, property);
+      }
+
+      if (property === StakenetPropertyEnum.userData) {
+        return await this.stakenet.getUserData(address, user);
+      }
+
+      if (property in StakenetPropertyWithRequiredUser) {
+        return await this.stakenet.getPropertyWithUserRequired(
+          address,
+          property,
+          user,
+        );
+      }
+
+      if (
+        property in StakenetPropertyWithOwner &&
+        property in StakenetPropertyWithSpender
+      ) {
+        return await this.stakenet.getPropertyWithOwnerAndSpender(
+          address,
+          property,
+          owner,
+          spender,
+        );
+      }
+    } catch (error) {
+      throw new BadRequestException(
+        'Ethereum address is not an Stakenet contract',
+      );
+    }
+    return true;
   }
 }
