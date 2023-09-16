1f1bd0f22dd029f6659fe5bdd359ee934be794f5
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-16 13:24:03 +0300

    feat: mock Web3 service in StakenetService test

--- src/stakenet/test/stakenet.service.spec.ts
+++ src/stakenet/test/stakenet.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StakenetService } from '../stakenet.service';
+import { Web3Service } from '../../web3/web3.service';
+
+const web3ServiceMock = {};
 
 describe('StakenetService', () => {
   let service: StakenetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StakenetService],
+      providers: [
+        StakenetService,
+        {
+          provide: Web3Service,
+          useValue: web3ServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<StakenetService>(StakenetService);
