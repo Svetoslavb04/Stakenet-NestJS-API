203e016b1ce94b90ebafc12a63d9a60d5524e0b7
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-19 15:07:46 +0300

    feate: added getUserData function in StakenetService

--- src/stakenet/stakenet.service.ts
+++ src/stakenet/stakenet.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Web3Service } from '../web3/web3.service';
 import { Stakenet, Stakenet__factory } from '../../types/ethers-contracts';
+import { ethers } from 'ethers';
 
 @Injectable()
 export class StakenetService {
@@ -52,5 +53,27 @@ export class StakenetService {
     throw new Error('The contract does not have such function');
   }
 
-  async getUserData(address: string, user: string) {}
+  async getUserData(address: string, user: string) {
+    const stakenet = this.createStakenetContract(address);
+
+    const balance = await stakenet.balanceOf(user);
+    const userHasStaked = await stakenet.userHasStaked(user);
+    const userStakedTimestamp = await stakenet.userStakedTimestamp(user);
+    const lockDurationInSeconds = await stakenet.lockDurationInSeconds();
+
+    const yieldPercentage = await stakenet.yieldPercentage();
+    const yieldDecimals = await stakenet.yieldDecimals();
+
+    const reward =
+      (balance * yieldPercentage) / ethers.parseUnits('100', yieldDecimals);
+
+    return {
+      balance,
+      reward,
+      lockDurationInSeconds,
+      userHasPosition: balance > 0,
+      userHasStaked,
+      userStakedTimestamp,
+    };
+  }
 }
